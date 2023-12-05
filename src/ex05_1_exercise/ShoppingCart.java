47820a677c3acbad0937dca5be6a7a16f9d0b4fe
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-12-05 12:25:53 +0200

    first commit

--- src/ex05_1_exercise/ShoppingCart.java
+++ src/ex05_1_exercise/ShoppingCart.java
@@ -2,14 +2,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package lesson5;
+package ex05_1_exercise;
 
 /**
  *
  * @author Jada
  */
-public class Lesson5 {
+public class ShoppingCart {
     public static void main(String[] args) {
+        String custName = "Jada";
+        String itemDesc = "Bar One Chocolate";
+        
+        boolean outOfStock = false;
+        double price = 9.99, tax = 0.15, total;
+        int quantity = 2;
+        
+        total = (price + tax) * quantity;
+        String message;
+        
+        if(quantity > 1) {
+         message = custName + " wants to purchase " + quantity + " " + itemDesc
+        + "s. Total cost with tax is: R" + total + ".";
+        }else {
+        message = custName + " wants to purchase " + quantity + " " + itemDesc
+        + " . Total cost with tax is: R" + total + ".";
+        }
+        
+        if(outOfStock == true) {
+        message = "Sorry but this item is out of stock";
+        }
+        
+        
+        System.out.println(message);
+        
         
     }
 }
+//Exercise 5-1:
+//
+//1. Declare a boolean, outOfStock, and initialize it to true.
+//2. Use an if statement to test the value of quantity.  
+//    If it is > 1, concatenate an 's' to message so that it indicates multiple items.
+//3. Use an if | else statement that tests the value of outOfStock.
+//    If true, let the user know the item is unavailable.
+//    If false, print the message variable, then print the total cost with tax.
+//4. Run with outOfStock = true and then again with outOfStock = false.
---
+++ src/ex05_2_exercise/ShoppingCart.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex05_1_exercise;
+
+/**
+ *
+ * @author Jada
+ */
+public class ShoppingCart {
+    public static void main(String[] args) {
+        String custName = "Jada";
+        String[] item = new String[4];
+        item[0] = "Bar One Chocolate";
+        item[1] = "Top Deck Chocolate";
+        item[2] = "LunchBar Chocolate";
+        item[3] = "Five Star Chocolate";
+                
+        //stock and pricing
+        boolean outOfStock = false;
+        double price = 9.99, tax = 0.15, total;
+        int quantity = item.length;
+        
+        total = (price + tax) * quantity;
+        //messages
+        String message;
+         message = custName + " wants to purchase " + quantity + " items.";
+
+        if(outOfStock == true) {
+        message = "Sorry but this item is out of stock";
+        }
+
+        System.out.println(message);
+ 
+    }
+}
+//Exercise 5-2
+//
+//1. Declare a String array and initialize it with 4 elements.
+//     Each element represents a different item description ("Shirt", for instance).
+//2. Change message to show how many items the customer wants to purchase 
+//     (Use the length property of the items array).
+//3. Print just one element of the items array.  
+//     What happens if you use index number 4?

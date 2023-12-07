3876c00597d26c48ac805b431c94db9d7e1880e3
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-12-07 19:11:18 +0200

    update

--- src/soccer/League.java
+++ src/soccer/League.java
@@ -10,6 +10,7 @@ package soccer;
  */
 public class League {
     public static void main(String[] args) {
+        //*********************TEAM 1 (THE GREENS)*****************************
         //instantiating objects
         Player player1 = new Player();
         Player player2 = new Player();
@@ -36,7 +37,22 @@ public class League {
         for(Player thePlayer: team1.playerArray) {
             System.out.println(thePlayer.playerName);
         }
+        //*********************TEAM 2 (THE REDS)*****************************
+        Team team2 = new Team();
+        team2.teamName = "The Reds";
         
+        team2.playerArray = new Player[3];
+        team2.playerArray[0] = new Player();
+        team2.playerArray[0].playerName = "Robert Service";
+        team2.playerArray[1] = new Player();
+        team2.playerArray[1].playerName = "Robbie Burns";
+        team2.playerArray[2] = new Player();
+        team2.playerArray[2].playerName = "Rafael Sabatini";
         
+        //team2
+        System.out.println(team2.teamName + " Players: ");
+        for(Player thePlayer: team2.playerArray) {
+            System.out.println(thePlayer.playerName);
+        }
     }
 }

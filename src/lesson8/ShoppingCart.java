92f7414f4ba0372bfb7f83fcf6d48a80702752e2
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-12-07 09:47:52 +0200

    update

--- src/lesson8/ShoppingCart.java
+++ src/lesson8/ShoppingCart.java
@@ -9,18 +9,36 @@ package lesson8;
  * @author Jada
  */
 public class ShoppingCart {
+
     public static void main(String[] args) {
-      Shirt myShirt = new Shirt();
-      myShirt.display();
+        Shirt myShirt = new Shirt();
+        Shirt yourShirt = new Shirt("long sleeve",'R',99.90);
+        myShirt.display();
+        yourShirt.display();
     }
 }
 
 class Shirt {
 
-    public String desc = "-description required-";
+    //Fields
+    public String desc;
     public char colourCode;
     public double price;
-    
+
+    //constructor(allows for values/params to be passed into fields)
+    //instantiates fields
+    public Shirt() {
+        desc = "-description required-";
+        colourCode = 'U';
+        price = 0.0;
+    }
+    //allows us to pass values during main method instantiation
+    public Shirt(String desc, char colourCode, double price) {
+        this.desc = desc;
+        this.colourCode = colourCode;
+        this.price = price;
+    }
+
     public void display() {
         System.out.println("Item description: " + desc);
         System.out.println("Colour Code: " + colourCode);

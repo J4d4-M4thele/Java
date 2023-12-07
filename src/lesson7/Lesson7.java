c8f991c1dfee042a214b4fd21fe2ff30cf284621
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-12-07 07:55:16 +0200

    first commit

---
+++ src/lesson7/Lesson7.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson7;
+
+/**
+ *
+ * @author Jada
+ */
+public class Lesson7 {
+
+    public static void main(String[] args) {
+//        String hello = "Hello World!!!!";
+//        int stringLength = hello.length();
+//        int hello2 = "Hi! How are you doing?".length();
+//        System.out.println(stringLength);
+//        System.out.println(hello2);
+        //trim() removes whitespace
+        String greet = " HOW ".trim();
+        String lc = (greet + "DY").toLowerCase();
+        System.out.println(lc);
+    }
+}

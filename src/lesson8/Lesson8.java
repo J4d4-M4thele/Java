5f4841f538cc4c5741bd4773b2f16ed677a9d0bc
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-12-07 10:26:07 +0200

    update

--- src/lesson8/Lesson8.java
+++ src/lesson8/Lesson8.java
@@ -9,8 +9,45 @@ package lesson8;
  * @author Jada
  */
 public class Lesson8 {
+
     public static void main(String[] args) {
-        
+//        Calculator calc = new Calculator();
+//        double den = 2.0;
+//
+//        calc.calculate(3, den);
+//        calc.printString();
+//        System.out.println(calc.sum(4, 6));
+//        System.out.println(calc.isGreater(20, 5));
+
+//        int num1 = 1, num2 = 2;
+//        int result = num1 + num2;
+//        System.out.println(result);
+        int num1 = 1, num2 = 2;
+        int result = sum(num1, num2);
+        System.out.println(result);
     }
- 
+    //static keyword allows us to access method without creating an instance of object
+    public static int sum(int x, int y) {
+        return (x + y);
+    }
+
+}
+
+class Calculator {
+
+    public void calculate(int x, double y) {
+        System.out.println(x / y);
+    }
+
+    public void printString() {
+        System.out.println("Hello");
+    }
+
+    public int sum(int x, int y) {
+        return (x + y);
+    }
+
+    public boolean isGreater(int x, int y) {
+        return (x > y);
+    }
 }

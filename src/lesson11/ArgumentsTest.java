ca648ac3953bef3b4cca03e4da4ced84a56e3041
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-12-12 06:50:20 +0200

    update

--- src/lesson11/ArgumentsTest.java
+++ src/lesson11/ArgumentsTest.java
@@ -9,5 +9,8 @@ package lesson11;
  * @author Jada
  */
 public class ArgumentsTest {
-    
+    System.out.println("Total is " + (args[0] + args[1]));
+        int arg1 = Integer.parseInt(args[0]);
+        int arg2 = Integer.parseInt(args[1]);
+        System.out.println("Total is " + (arg1 + arg2));
 }
